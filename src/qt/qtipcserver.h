61136ae989fcabb53646199582669d471cec49c4
geekcoindev <geekcoin@hotmail.com>
2014-11-24 04:04:15 +0400

    Launch

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Geekcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GeekcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
